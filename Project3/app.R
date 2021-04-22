5f2e50241854338a1c3821b263882a5c780f9a93
Bhatt <vivekb2@uic.edu>
2021-04-22 00:29:00 -0500

    push app.r

--- Project3/app.R
+++ Project3/app.R
@@ -51,6 +51,81 @@ ui <- dashboardPage(
                           leafletOutput("west_loop_map", height = 630)
                   )
                 )
+            ),
+            
+            tabItem(
+                tabName="community_compare",
+                column(6, 
+                       fluidRow(
+                            column(4,
+                                    
+                                   selectizeInput(
+                                       'com1_view', 'Select a View: ', choices = c("Electricity", "Gas", "Building Type", "Building Age", "Building Height", "Total Population"), selected = "Electricity", multiple = FALSE
+                                   ),
+                                   
+                                   selectizeInput(
+                                       'com1_months', 'Select a Time Frame: ', choices = c("Year", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"), selected = "Year", multiple = TRUE
+                                   ),
+                                   
+                                   selectizeInput(
+                                       'com1_building', 'Select a Building Type: ', choices = c("All", "Commercial", "Residential", "Industrial"), selected = "All", multiple = TRUE
+                                   ),
+                                   
+                                   actionButton("reset_com1", "Reset View")       
+                                   
+                           ), 
+                           
+                           column(8, 
+                                  
+                                  leafletOutput("com1_map", height = 630)
+                                  
+                                  )
+                       )
+               ),
+            
+            
+            column(6, 
+                   fluidRow(
+                       column(4,
+                              
+                              selectizeInput(
+                                  'com2_view', 'Select a View: ', choices = c("Electricity", "Gas", "Building Type", "Building Age", "Building Height", "Total Population"), selected = "Electricity", multiple = FALSE
+                              ),
+                              
+                              selectizeInput(
+                                  'com2_months', 'Select a Time Frame: ', choices = c("Year", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"), selected = "Year", multiple = TRUE
+                              ),
+                              
+                              selectizeInput(
+                                  'com2_building', 'Select a Building Type: ', choices = c("All", "Commercial", "Residential", "Industrial"), selected = "All", multiple = TRUE
+                              ),
+                              
+                              actionButton("reset_com2", "Reset View")       
+                              
+                       ), 
+                       
+                       column(8, 
+                              
+                              leafletOutput("com2_map", height = 630)
+                              
+                       )
+                   )
+                )
+            
+            ),
+            
+            tabItem(
+                tabName = "chicago",
+                column(2, 
+                       
+                       selectizeInput(
+                           'chicago_view', 'Select a View: ', choices = c("Electricity", "10% Most Electricity", "Gas", "10% Most Gas", "Building Type", "Building Age", "10% Most Oldest Buildings", "10% Most Oldest Buildings", "Building Height", "10% Most Building Height", "10% Most Gas", "Total Population", "10% Most Populated", "10% Most Occupied", "10% Most Renters"), selected = "Electricity", multiple = FALSE
+                       ),
+                )
+            ),
+            
+            tabItem(
+                tabName = "about"
             )
         )
     )
